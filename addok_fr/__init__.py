f1e135d1078e5499407a9af23014b34b238dc5d9
Yohan Boniface <yb@enix.org>
2017-02-09 18:59:08 +0100

    French synonyms are now here

--- addok_fr/__init__.py
+++ addok_fr/__init__.py
@@ -1,5 +1,13 @@
+from pathlib import Path
+
 from addok.helpers import yielder
 
 from . import utils
 
 phonemicize = yielder(utils.phonemicize)
+
+RESOURCES_ROOT = Path(__file__).parent / 'resources'
+
+
+def preconfigure(config):
+    config.SYNONYMS_PATH = RESOURCES_ROOT / 'synonyms.txt'
